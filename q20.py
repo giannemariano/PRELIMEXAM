60a0e016896086fbf627aab304881100f497fb83
giannemariano <qgcimariano@tip.edu.ph>
2023-02-27 09:46:22 +0000

    20

---
+++ q20.py
@@ -0,0 +1,15 @@
+import unittest
+from unittest.case import TestCase
+
+def multiply(x,y):    
+    return x*y 
+
+class TestMultiply(unittest.TestCase):
+
+    def test(self):
+
+        self.assertEqual(multiply(32 - 32, 0.26),0)
+
+if __name__ == '__main__':
+
+ unittest.main()
