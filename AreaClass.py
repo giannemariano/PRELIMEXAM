2c139b0c8de9053d9796a3340463d6e9f1e2f8c0
giannemariano <qgcimariano@tip.edu.ph>
2023-02-27 10:15:08 +0000

    klas

---
+++ AreaClass.py
@@ -0,0 +1,9 @@
+class Area:
+    def __init__(self, length, width, radius):
+        length = 20
+        width = 0
+        radius = 0
+
+    def __str__(self):
+        return f"{self.circle}, {self.width}, {self.radius}"
+        
---
+++ rec_circ.py
@@ -0,0 +1,11 @@
+import AreaClass
+
+length = 20
+width = 0
+radius = 0
+
+circle = (length*width)
+rectangle = (radius)
+
+print("Circle area equals to: ", circle)
+print("Rectangle area equals to: ", rectangle)
